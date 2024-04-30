01004a293f8a8b51a5fc0e29f0302ce462313589
justinmcarlson <29412057+justinmcarlson@users.noreply.github.com>
2024-04-29 21:40:49 -0500

    Add Edit Recipe Component

--- src/Container/App.jsx
+++ src/Container/App.jsx
@@ -170,32 +170,12 @@ export default function App() {
         ))}
       </ul>
 
-      <form onSubmit={handleEditRecipeSubmit}>
-        <h2>Edit Recipe</h2>
-        <label htmlFor="name">Name:</label>
-        <input
-          name="name"
-          placeholder="Edit Recipe"
-          value={editRecipe.name || ''}
-          onChange={handleEditRecipeChange}
-          required
-        />
-        <br />
-        <label htmlFor="ingredients">Ingredients (separate by commas):</label>
-        <br />
-        <textarea
-          name="ingredients"
-          placeholder="Ingredients..."
-          value={editRecipe.ingredients || ''}
-          onChange={handleEditRecipeChange}
-          required
-        />
-        <br />
-        <button type="submit">Save Recipe</button>
-      </form>
-
-      {/* <EditRecipe />
-      <NewRecipe /> */}
+      <EditRecipe 
+      handleEditRecipeChange={handleEditRecipeChange}
+      handleEditRecipeSubmit={handleEditRecipeSubmit}
+      editRecipe={editRecipe}
+      />
+      {/* <NewRecipe /> */}
 
       <IngredientsList allRecipes={allRecipes} />
     </main>
