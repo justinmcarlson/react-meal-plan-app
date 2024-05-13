fe6baecb33169084faa9dc751b6842af4c9555f0
justinmcarlson <29412057+justinmcarlson@users.noreply.github.com>
2024-05-12 21:17:44 -0500

    Wrap NewRecipe Component In HTML Dialog

--- src/Presentational/NewRecipe.jsx
+++ src/Presentational/NewRecipe.jsx
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 export default function NewRecipe({
   handleNewRecipeSubmit,
   handleNewRecipeChange,
   newRecipe,
 }) {
+  function closeModal() {
+    const dialog = document.getElementById("newRecipeDialog");
+    dialog.close();
+  }
+  function openModal() {
+    const dialog = document.getElementById("newRecipeDialog");
+    dialog.showModal();
+  }
   return (
     <>
-      <form onSubmit={handleNewRecipeSubmit}>
-        <h2>Add New Recipe</h2>
-        <label htmlFor="name">Name:</label>
-        <input
-          name="name"
-          placeholder="New Recipe"
-          value={newRecipe.name || ''}
-          onChange={handleNewRecipeChange}
-          required
-        />
-        <br />
-        <label htmlFor="ingredients">Ingredients (separate by commas):</label>
-        <br />
-        <textarea
-          name="ingredients"
-          placeholder="Ingredients..."
-          value={newRecipe.ingredients || ''}
-          onChange={handleNewRecipeChange}
-          required
-        />
-        <br />
-        <button type="submit">Add Recipe</button>
-      </form>
+      <dialog id="newRecipeDialog">
+        <form onSubmit={handleNewRecipeSubmit}>
+          <h2>Add New Recipe</h2>
+          <label htmlFor="name">Name:</label>
+          <input
+            name="name"
+            placeholder="New Recipe"
+            value={newRecipe.name || ""}
+            onChange={handleNewRecipeChange}
+            required
+          />
+          <br />
+          <label htmlFor="ingredients">Ingredients (separate by commas):</label>
+          <br />
+          <textarea
+            name="ingredients"
+            placeholder="Ingredients..."
+            value={newRecipe.ingredients || ""}
+            onChange={handleNewRecipeChange}
+            required
+          />
+          <br />
+          <button type="submit" >Add Recipe</button>
+          <button id="closeNewRecipeDialog" onClick={closeModal} autofocus>Close</button>
+        </form>
+      </dialog>
+      <button id="OpenNewRecipeDialog" onClick={openModal}>Add Recipe</button>
     </>
   );
 }
