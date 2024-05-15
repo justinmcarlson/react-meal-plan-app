4d48cb4e94d151158a0a74dd06afcdfbe934ca1f
justinmcarlson <29412057+justinmcarlson@users.noreply.github.com>
2024-05-14 21:33:22 -0500

    Add Basic Layout and Styles to RecipeList Component

--- src/Presentational/RecipeList.jsx
+++ src/Presentational/RecipeList.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
+import styles from "./RecipeList.module.css";
 
 export default function RecipeList({
   allRecipes,
   handleSortByName,
   handleEditRecipe,
   handleDelete,
-  handleCheckedChange
+  handleCheckedChange,
 }) {
   return (
     <>
       <h2>Recipes</h2>
       <button onClick={handleSortByName}>Sort Recipes</button>
-      <ul className="recipe-list">
+      <ul className={styles.recipeList}>
         {allRecipes.map((recipe) => (
           <li key={recipe.id}>
             <input
@@ -19,9 +20,12 @@ export default function RecipeList({
               checked={recipe.checked}
               onChange={(e) => handleCheckedChange(recipe.id, e.target.checked)} // Pass recipeId
             />
-            {recipe.name}
-            <button onClick={() => handleEditRecipe(recipe)}>Edit</button>
-            <button onClick={() => handleDelete(recipe.id)}>X</button>
+            <span className={styles.recipeName}>{recipe.name}</span>
+            <span className={styles.recipeActions}>
+              <span className={styles.recipeRating}>{recipe.rating}⭐</span>
+              <button onClick={() => handleEditRecipe(recipe)}>Edit</button>
+              <button onClick={() => handleDelete(recipe.id)}>X</button>
+            </span>
           </li>
         ))}
       </ul>
