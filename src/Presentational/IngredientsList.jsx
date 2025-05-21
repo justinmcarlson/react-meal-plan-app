ea90074b72094d031a6d46bf28ad8e6459a787e9
justinmcarlson <29412057+justinmcarlson@users.noreply.github.com>
2025-05-20 21:40:40 -0500

    Add copy to clipboard function

--- src/Presentational/IngredientsList.jsx
+++ src/Presentational/IngredientsList.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function IngredientsList({ allRecipes, openModal, closeModal }) {
+  const [copied, setCopied] = useState(false);
   const checkedRecipes = allRecipes.filter((recipe) => recipe.checked);
   const ingredientsList = checkedRecipes.map((recipe) => recipe.ingredients.join('\n'));
   return (
@@ -17,7 +18,16 @@ export default function IngredientsList({ allRecipes, openModal, closeModal }) {
         ))}
       </ul>
       <div className="flex justify-end">
-        <button className="bg-lime-600 hover:bg-lime-700 text-white py-2 px-4 rounded-full" onClick={() => navigator.clipboard.writeText(ingredientsList) }>Copy to Clipboard</button>
+        <button 
+          className="bg-lime-600 hover:bg-lime-700 text-white py-2 px-4 rounded-full" 
+          onClick={() => {
+            navigator.clipboard.writeText(ingredientsList);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+          }}
+        >
+          Copy to Clipboard {copied && '👍'}
+        </button>
         <button className="bg-gray-100 hover:bg-gray-300 py-2 px-4 rounded-full" onClick={() => closeModal('ingredientsListDialog')}>Close</button>
       </div>
       </dialog>
